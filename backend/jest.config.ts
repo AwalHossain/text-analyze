d218ebd79d85bbda7064dfd03df1a9d145d53137
Awalhossain <waqin21@gmail.com>
2025-02-22 11:20:40 +0600

    test(jest): configure Jest testing framework for backend

---
+++ backend/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@utils/(.*)$': '<rootDir>/common/utils/$1',
+  },
+  // Add these settings
+  moduleDirectories: ['node_modules', 'src'],
+  setupFiles: ['<rootDir>/test/jest-setup.ts'],
+};
+
+export default config;
